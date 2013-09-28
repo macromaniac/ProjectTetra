6cbbe2459075876e01bae34c3b0b65015f9647bb
EricHornby <eric_hermit@hotmail.com>
2013-09-28 16:49:05 -0500

    Disclaimer

--- ProjectTetra/ProjectTetra/Game1.cs
+++ ProjectTetra/ProjectTetra/Game1.cs
@@ -9,6 +9,27 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Input.Touch;
 using Microsoft.Xna.Framework.Media;
+
+
+/*
+ * Project Tetra
+ * 
+ * Created 9/28/2013 at Texas A&M's Hackathon by Eric Hornby and Joey Caero.
+ * 
+ * We started this project in thruth at around 2 AM after spending forever to figure out how to set set up our development environments and get it working 
+ * on a phone. Then proceeded an exhausted and mad attempt to give substance to our ideas for a fun game for Windows Phone ending at 1:30 PM. 
+ * 
+ * All semblance of code quality was thrown out the window. I was ready to go to bed before we had even started the programming. Joey and I are planning on
+ * rewriting much of those code, but with actual code quality as a concern. As of now, X doesnt always mean x and y doesnt always mean y. There are even
+ * worse things than that hiding around.
+ * 
+ * So basically, if you're looking at this, feel free to laugh in hilarity at the abominations you see within and please do not use this as an example
+ * of good code!
+ * 
+ * Cheers!
+ * 
+ * */
+
 namespace ProjectTetra
 {
 
